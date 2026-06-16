5936a45f8aedd77cc0f97e7b56c733fa9f0a099d
hnkrs45 <haquenawazkhan007@gmail.com>
2026-06-16 08:06:18 +0530

    16th june second commit

---
+++ AssignCookies.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int ans=0;
+        int i=0, j=0;
+        while(i<g.length && j<s.length){
+            if(s[j]>=g[i]){
+                ans++;
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+        return ans;
+    }
+}
