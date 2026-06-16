f7b15053806e87bce851de148544f9fe5fe89510
hnkrs45 <haquenawazkhan007@gmail.com>
2026-06-16 08:11:00 +0530

    16th june fourth commit

---
+++ Subsetsii.java
@@ -0,0 +1,17 @@
+class Solution {
+    void solve(int index, int[] nums, List<Integer> ds, List<List<Integer>> ans){
+        ans.add(new ArrayList<>(ds));
+        for(int i=index;i<nums.length;i++){
+            if(i!=index && nums[i]==nums[i-1]) continue;
+            ds.add(nums[i]);
+            solve(i+1, nums, ds, ans);
+            ds.remove(ds.size()-1);
+        }
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans=new ArrayList<>();
+        solve(0, nums, new ArrayList<>(), ans);
+        return ans;
+    }
+}
