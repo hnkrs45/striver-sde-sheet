06b23ae33b51f2accff722ba17232722f6cdbb5f
hnkrs45 <haquenawazkhan007@gmail.com>
2026-06-18 16:55:48 +0530

    18th June first commit

---
+++ PermutaionsOfAString.java
@@ -0,0 +1,19 @@
+class Solution {
+    void solve(List<List<Integer>> ans, List<Integer> temp,int nums[]){
+        if(temp.size()==nums.length){
+            ans.add(new ArrayList<>(temp));
+            return ;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(temp.contains(nums[i])) continue;
+            temp.add(nums[i]);
+            solve(ans,temp,nums);
+            temp.remove(temp.size()-1);
+        }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        solve(ans,new ArrayList<>(),nums);
+        return ans;
+    }
+}
