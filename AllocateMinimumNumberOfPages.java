a96a09cc8ce191026898dc5ac40536a991157943
hnkrs45 <haquenawazkhan007@gmail.com>
2026-06-21 06:22:30 +0530

    21st june third commit

---
+++ AllocateMinimumNumberOfPages.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Solution{
+    public static int countStudents(ArrayList<Integer> arr, int pages){
+        int n=arr.size();
+        int students=1;
+        long pagesStudent=0;
+        for(int i=0;i<n;i++){
+            if(pagesStudent+arr.get(i)<=pages){
+                pagesStudent+=arr.get(i);
+            }
+            else{
+                students++;
+                pagesStudent=arr.get(i);
+            }
+        }
+        return students;
+    }
+
+    public static int findPages(ArrayList<Integer> arr, int n, int m){
+        if(m>n)
+            return -1;
+
+        int low=Collections.max(arr);
+        int high=arr.stream().mapToInt(Integer::intValue).sum();
+        while(low<=high){
+            int mid=(low+high)/2;
+            int students=countStudents(arr, mid);
+            if(students>m){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return low;
+    }
+}
+public class AllocateMinimumNumberOfPages{
+    public static void main(String[] args){
+        ArrayList<Integer> arr=new ArrayList<>(Arrays.asList(25, 46, 28, 49, 24));
+        int n=5;
+        int m=4;
+        Solution sol=new Solution();
+        int ans=sol.findPages(arr, n, m);
+        System.out.println("The answer is: " + ans);
+    }
+}
