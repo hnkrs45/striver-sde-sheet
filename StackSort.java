979d5c1cfea8dae93f1cc6ce24296bd2ecd38e20
hnkrs45 <haquenawazkhan007@gmail.com>
2026-06-25 07:26:49 +0530

    25th june third commit

---
+++ StackSort.java
@@ -0,0 +1,42 @@
+import java.util.Stack;
+
+class Solution{
+    public void insert(Stack<Integer> stack, int temp){
+        if(stack.isEmpty() || stack.peek()<=temp){
+            stack.push(temp);
+            return;
+        }
+        int val=stack.pop();
+        insert(stack, temp);
+        stack.push(val);
+    }
+
+    public void sortStack(Stack<Integer> stack){
+        if(!stack.isEmpty()){
+            int temp=stack.pop();
+            sortStack(stack);
+            insert(stack, temp);
+        }
+    }
+}
+
+public class StackSort{
+    public static void main(String[] args){
+        Solution sol=new Solution();
+        Stack<Integer> stack=new Stack<>();
+        stack.push(4);
+        stack.push(1);
+        stack.push(3);
+        stack.push(2);
+        stack.push(5);
+        stack.push(2);
+        stack.push(9);
+        stack.push(7);
+        stack.push(8);
+        sol.sortStack(stack);
+        System.out.print("Sorted stack (descending order): ");
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
+        }
+    }
+}
