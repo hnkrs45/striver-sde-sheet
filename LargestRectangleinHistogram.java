f6aa1a6adf9dd55c602ef1388cded999fd1fc3af
hnkrs45 <haquenawazkhan007@gmail.com>
2026-06-27 08:16:45 +0530

    27th june first commit updated

--- LargestRectangleinHistogram.java
+++ LargestRectangleinHistogram.java
@@ -1,60 +1,28 @@
 class pair{
-    int i;
-    int j;
-    int time;
-    pair(int i, int j, int time){
-        this.i=i;
-        this.j=j;
-        this.time=time;
+    int index;
+    int height;
+    pair(int i, int h){
+        this.index=i;
+        this.height=h;
     }
 }
 class Solution {
-    public int orangesRotting(int[][] grid) {
-        int n=grid.length, m=grid[0].length;
-        Queue<pair> q=new LinkedList<>();
-        int vis[][]=new int[n][m];
-        int countfresh=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
-                    q.add(new pair(i, j, 0));
-                    vis[i][j]=2;
-                }
-                else{
-                    vis[i][j]=0;
-                }
-                if(grid[i][j]==1) countfresh++;
+    public int largestRectangleArea(int[] heights) {
+        Stack<pair> st = new Stack<pair>();
+        int ans=0;
+        for(int i=0;i<heights.length;i++){
+            int start=i;
+            while(!st.isEmpty() && st.peek().height>heights[i]){
+                pair p = st.pop();
+                ans=Math.max(ans,p.height*(i-p.index));
+                start=p.index;
             }
+            st.add(new pair(start,heights[i]));
         }
-        int ans=0, count=0;
-        while(!q.isEmpty()){
-            int row=q.peek().i;
-            int col=q.peek().j;
-            int time=q.peek().time;
-            ans=Math.max(ans, time);
-            q.remove();
-            if(row>0 && grid[row-1][col]==1 && vis[row-1][col]==0){
-                q.add(new pair(row-1, col, time+1));
-                vis[row-1][col]=1;
-                count++;
-            }
-            if(col>0 && grid[row][col-1]==1 && vis[row][col-1]==0){
-                q.add(new pair(row, col-1, time+1));
-                vis[row][col-1]=1;
-                count++;
-            }
-            if(row<n-1 && grid[row+1][col]==1 && vis[row+1][col]==0){
-                q.add(new pair(row+1, col, time+1));
-                vis[row+1][col]=1;
-                count++;
-            }
-            if(col<m-1 && grid[row][col+1]==1 && vis[row][col+1]==0){
-                q.add(new pair(row, col+1, time+1));
-                vis[row][col+1]=1;
-                count++;
-            }
+        while(!st.isEmpty()){
+            pair p=st.pop();
+            ans = Math.max(ans, p.height* (heights.length-p.index));
         }
-        if(count!=countfresh) return -1;
         return ans;
     }
 }
