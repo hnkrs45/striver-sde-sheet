dc83f650d516ee4d574e2c1f7238f3586d2f30bd
hnkrs45 <haquenawazkhan007@gmail.com>
2026-06-28 07:53:42 +0530

    28th june first commit

---
+++ OnlineStockSpan.java
@@ -0,0 +1,15 @@
+class StockSpanner {
+    Stack<int[]> st;
+    public StockSpanner() {
+        st=new Stack();
+    }
+    
+    public int next(int price) {
+        int ans=1;
+        while(!st.isEmpty() && st.peek()[0]<=price){
+            ans+=st.pop()[1];
+        }
+        st.add(new int[]{price, ans});
+        return ans;
+    }
+}
