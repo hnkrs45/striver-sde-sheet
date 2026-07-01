0aaaaa60187e8346de83839387167f2b9f3c0e5e
hnkrs45 <haquenawazkhan007@gmail.com>
2026-07-01 07:53:34 +0530

    1st july third commit

---
+++ MinimumIntersectionStepsToMakeAStringPalindrome.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int lcs(String s1, String s2){
+        int n=s1.length();
+        int m=s2.length();
+        int[] prev=new int[m+1];
+        int[] cur=new int[m+1];
+        for(int ind1=1;ind1<=n;ind1++){
+            for(int ind2=1;ind2<=m;ind2++){
+                if(s1.charAt(ind1-1)==s2.charAt(ind2-1))
+                    cur[ind2]=1+prev[ind2-1];
+                else
+                    cur[ind2]=Math.max(prev[ind2], cur[ind2-1]);
+            }
+            prev=cur.clone();
+        }
+        return prev[m];
+    }
+    public int lps(String s) {
+        StringBuilder sb=new StringBuilder(s);
+        sb.reverse();
+        String t=sb.toString();
+        return lcs(s, t);
+    }
+    public int minInsertions(String s) {
+        int n=s.length();
+        int temp=lps(s);
+        return n-temp;
+    }
+}
