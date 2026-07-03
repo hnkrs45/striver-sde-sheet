3a7c906ba20cee76edfea4b4dfaf775aae3f3204
hnkrs45 <haquenawazkhan007@gmail.com>
2026-07-03 07:21:45 +0530

    3rd july second commit

---
+++ BinaryTreePreOrderTraversal.java
@@ -0,0 +1,13 @@
+class Solution {
+    List<Integer> ans=new ArrayList<>();
+    void solve(TreeNode root){
+        if(root==null) return ;
+        ans.add(root.val);
+        solve(root.left);
+        solve(root.right);
+    }
+    public List<Integer> preorderTraversal(TreeNode root) {
+        solve(root);
+        return ans;
+    }
+}
